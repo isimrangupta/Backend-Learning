5f5d31f537501da470ffbef910fad4e635541faf
Diksha Gupta <simrandgupta95@gmail.com>
2026-04-23 20:26:34 +0530

    Authentication with JWT

---
+++ class-5/src/routes/auth.routes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const userModel = require("../models/user.model");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await userModel.create({
+    username,
+    password,
+  });
+
+  res.status(201).json({
+    message: "User registered Successfully !",
+    user,
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await userModel.findOne({
+    username: username
+  })
+
+  if (!user) {
+    return res.status(401).json({
+      message: "user account not found [Invalid username]",
+    })
+  }
+
+  const isPasswordValid = password == user.password
+  if(!isPasswordValid){
+    return res.status(401).json({
+        message:"Invalid Password"
+    })
+}
+
+res.status(200).json({
+    message:"user loggedIn successfully !"
+})
+});
+
+module.exports = router;
