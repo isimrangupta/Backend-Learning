2d62a02583edef5500adda261b8db8991044e2d7
Diksha Gupta <simrandgupta95@gmail.com>
2026-04-26 20:37:07 +0530

    feat: add JWT authentication with cookie-based token storage

--- class-5/src/routes/auth.routes.js
+++ class-5/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -11,6 +12,16 @@ router.post("/register", async (req, res) => {
     password,
   });
 
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  // server directly cookie mein token save kar sakta hai
+res.cookie("token", token)
+
   res.status(201).json({
     message: "User registered Successfully !",
     user,
@@ -21,25 +32,53 @@ router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   const user = await userModel.findOne({
-    username: username
-  })
+    username: username,
+  });
 
   if (!user) {
     return res.status(401).json({
       message: "user account not found [Invalid username]",
-    })
+    });
   }
 
-  const isPasswordValid = password == user.password
-  if(!isPasswordValid){
+  const isPasswordValid = password == user.password;
+  if (!isPasswordValid) {
     return res.status(401).json({
-        message:"Invalid Password"
-    })
-}
+      message: "Invalid Password",
+    });
+  }
 
-res.status(200).json({
-    message:"user loggedIn successfully !"
-})
+  res.status(200).json({
+    message: "user loggedIn successfully !",
+  });
+});
+
+
+router.get("/user", async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }   
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findOne({
+      _id:decoded.id
+    }).select("-password")
+
+    res.status(200).json({
+      Message: "user data fetched successfully !",
+      user
+    })
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized - Invalid token",
+    });
+  }
 });
 
 module.exports = router;
