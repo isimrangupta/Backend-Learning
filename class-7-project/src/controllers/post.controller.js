71921268ba38cc1dee46d56870c8e7551aa4a15c
Diksha Gupta <simrandgupta95@gmail.com>
2026-05-05 21:25:47 +0530

    Add image upload functionality

--- class-7-project/src/controllers/post.controller.js
+++ class-7-project/src/controllers/post.controller.js
@@ -1,20 +1,29 @@
-const postModel =require("../models/post.model");
+const postModel = require("../models/post.model");
 const generateCaption = require("../service/ai.service");
+const uploadFile = require("../service/storage.service");
+const { v4: uuidv4 } = require("uuid");
 
-async function createPostController(req,res) {
-    const file = req.file;
-    console.log("File received", file)
+async function createPostController(req, res) {
+  const file = req.file;
+  console.log("File received", file);
 
-    const base64Image = new Buffer.from(file.buffer).toString('base64');
+  const base64Image = new Buffer.from(file.buffer).toString("base64");
 
-    const caption = await generateCaption(base64Image);
+  const caption = await generateCaption(base64Image);
+  const result = await uploadFile(file.buffer, `${uuidv4()}`);
 
-    res.json({
-        caption
-    })
+  const post = await postModel.create({
+    caption: caption,
+    image:result.url,
+    user:req.user._id
+  });
 
+  res.status(201).json({
+    message: "Post created successfully",
+    post
+  })
 }
 
 module.exports = {
-    createPostController
-}
+  createPostController,
+};
